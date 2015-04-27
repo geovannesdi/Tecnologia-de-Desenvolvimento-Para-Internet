16f371fa5a79be71ba46e2a7475510cea00fde38
Geovanne Barreto <geovannesdi@gmail.com>
2015-04-27 11:16:01 -0300

    Exercício C#, primeiro exercício.

---
+++ Exercício C#/MeuPrimeiroPrograma/MeuPrimeiroPrograma/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MeuPrimeiroPrograma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu nome ");
+
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Seja bem vindo: " + nome);
+
+            Console.WriteLine("Digite qualquer tecla para sair.");
+
+            Console.ReadKey();
+        }
+    }
+}
