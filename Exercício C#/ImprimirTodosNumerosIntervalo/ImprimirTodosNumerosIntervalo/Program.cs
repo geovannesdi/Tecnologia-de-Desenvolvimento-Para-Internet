06af6f3e5c270f0ad9da181821cccb97503ef0b5
Geovanne Barreto <geovannesdi@gmail.com>
2015-04-27 12:20:04 -0300

    Resolução de mais dois exercícios sobre c#.

---
+++ Exercício C#/ImprimirTodosNumerosIntervalo/ImprimirTodosNumerosIntervalo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ImprimirTodosNumerosIntervalo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numeroUm, numeroDois;
+            
+            Console.WriteLine("DIGITE OS DOIS NUMERO PARA SE MOSTRAR TODOS OS NUMEROS ENTRE ELES?");
+
+            numeroUm = int.Parse(Console.ReadLine());
+
+            numeroDois = int.Parse(Console.ReadLine());
+
+            if (numeroUm <= numeroDois)
+            {
+                for (int i = 0; i < numeroDois-numeroUm; i++)
+                {
+                    Console.WriteLine(numeroUm+i);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < numeroUm - numeroDois; i++)
+                {
+                    Console.WriteLine((numeroUm - numeroDois) + i);
+                }
+            }
+
+            Console.WriteLine("DIGITE QUELUER TECLA PARA SAIR.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exercício C#/ManipulaçãoNumerica/ManipulaçãoNumerica/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ManipulaçãoNumerica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numeroUm, numeroDois;
+            
+            Console.WriteLine("Digite dois numeros, para serem manipulados");
+            
+            numeroUm = int.Parse(Console.ReadLine());
+                 
+            numeroDois = int.Parse(Console.ReadLine());
+
+            
+            Console.WriteLine("Numeros digitados "+ numeroUm + " "+ numeroDois);
+
+            Console.WriteLine("===============================================");
+
+            Console.WriteLine("Soma ===> "+ (numeroUm+numeroDois));
+            Console.WriteLine("Multiplicação ===> " + (numeroUm * numeroDois));
+            Console.WriteLine("Subtração ===> " + (numeroUm - numeroDois));
+
+            if (numeroDois == 0 || numeroDois < 0)
+            {
+                Console.WriteLine("Não pode dividir por 0 ou menor que zero");
+            }
+            else
+            {
+                 Console.WriteLine("Divisão ===> " + (numeroUm / numeroDois));
+            }
+            Console.WriteLine("===============================================");
+            Console.WriteLine("DIGITE QUELUER TECLA PARA SAIR.");
+            Console.ReadKey();
+        }
+    }
+}
